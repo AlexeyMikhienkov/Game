74e5421fc2c4df7141265bad984a7acb7300f327
AlexeyMikhienkov <a.mikhienkov@yandex.ru>
2021-08-31 16:43:35 +0300

    fix: исправлен перерендер уровня из-за изменения времени в компоненте Game

--- components/game/game.js
+++ components/game/game.js
@@ -1,28 +1,40 @@
 import Grid from "../grid/grid";
-import React, {useEffect, useState} from "react";
-import {colors, continueText, fingerParams, levels, tutorialData} from "../../constants/constants";
+import React, {useEffect, useState, useMemo} from "react";
+import {colors, continueText, levels, time, tutorialData} from "../../constants/constants";
 import {createRandomGrid} from "../../utils/createRandomGrid";
 import {rng} from "../../utils/rng";
 import GameTask from "./gameTask";
 import {useResult} from "../../hooks/resultObject";
 import Info from "../info/info";
+import {useTimer} from "../../hooks/timerObject";
 
 export default function Game({className, isTutorial, onAction, onChangePage}) {
     const [level, setLevel] = useState(0);
+    const baseLevelData = useMemo(() => {
+        const levelData = generateLevelData(level, isTutorial);
+        return {levelData, task: generateTaskValue(levelData.grid, isTutorial)}
+    }, []);
     const [result, setResult] = useResult();
-    const [data, setData] = useState(generateLevelData(level, isTutorial));
+    const [data, setData] = useState(baseLevelData.levelData);
+
     const [amount, setAmount] = useState(0);
     const [animation, setAnimation] = useState(null);
+    const [timer,] = useTimer(time);
 
     const {grid, color, size} = data;
+    const [value, setValue] = useState(baseLevelData.task);
+
     const rightImgSrc = "/images/right_answer.png";
     const wrongImgSrc = "/images/wrong_answer.png";
 
-    const value = isTutorial ? tutorialData.value : generateTaskValue(grid);
-
     useEffect(() => {
-        setTimeout(() => setAnimation(null), 750);
-        setData(generateLevelData(level, isTutorial));
+        console.log("render");
+        const timeout = setTimeout(() => setAnimation(null), 750);
+        const newData = generateLevelData(level, isTutorial);
+        const newValue = generateTaskValue(newData.grid, isTutorial);
+        setData(newData);
+        setValue(newValue);
+        return () => clearTimeout(timeout);
     }, [amount]);
 
     function checkAnswer(blockNumber, value, res) {
@@ -36,9 +48,7 @@ export default function Game({className, isTutorial, onAction, onChangePage}) {
         } else {
             setAnimation("wrong");
             res.all = res.all + 1;
-            console.log("before", res.combo);
             res.combo = Math.min(Math.max(res.combo - 1, 1), 5);
-            console.log("after", res.combo);
             setLevel(Math.min(Math.max(level - 1, 0), 8));
         }
         console.log(res);
@@ -48,7 +58,8 @@ export default function Game({className, isTutorial, onAction, onChangePage}) {
     return (
         <div className={`game ${isTutorial ? "game_tutorial" : ""} ${className ?? ""}`} onClick={onAction}
              style={{backgroundColor: color}}>
-            {!isTutorial ? <Info className={"game__info"} level={level} mult={result.combo} time={"01:00"}/> : null}
+            {!isTutorial ? <Info className={"game__info"} level={level} mult={result.combo}
+                                 time={`${Math.floor(timer / 60)}:${timer % 60}`}/> : null}
             <GameTask value={value} color={color}/>
             <Grid className={"game__grid"} grid={grid} size={size} value={value} isTutorial={isTutorial}
                   onCheckAnswer={(block, value) => {
@@ -67,6 +78,8 @@ export default function Game({className, isTutorial, onAction, onChangePage}) {
 }
 
 function generateLevelData(lvl, isTutorial) {
+    console.log("generate level data");
+
     return isTutorial ? tutorialData : {
         size: levels[lvl].size,
         grid: createRandomGrid(levels[lvl]),
@@ -74,8 +87,13 @@ function generateLevelData(lvl, isTutorial) {
     };
 }
 
-function generateTaskValue(grid) {
+function generateTaskValue(grid, isTutorial) {
+    console.log("generateTaskValue");
+
+    if (isTutorial) return tutorialData.value;
+
     const numbers = grid.flat().map(block => block.number);
+    console.log(numbers);
     const numIndex = rng.nextRange(0, numbers.length);
 
     return numbers[numIndex];
